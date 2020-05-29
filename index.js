d31ab50451891d779402364c2c4aedfaac44db84
Russell Salcido <russell.salcido@gmail.com>
2020-05-28 20:03:40 -0700

    Created prompt for git hub username

--- index.js
+++ index.js
@@ -9,20 +9,27 @@ inquirer
   })
   .then(function({ username }) {
     const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
+    console.log();
 
     axios.get(queryUrl).then(function(res) {
       const repoNames = res.data.map(function(repo) {
         return repo.name;
-        });
-    });    
-}); 
+        console.log(repo)
+      });
+
+    //   const repoData = ;
+
+    //   fs.writeFile("read-me.md", repoName, function(err) {
+    //     if (err) {
+    //       throw err;
+    //     }
+
+    //     console.log()
+    //   });
+    });
+  });
 
 // const questions = [
-//     {
-//         name: "Project Title"
-//         message:
-
-//     }
 
 // ];
 
