50c1dc54e45167a8be78f9979a3d453c04594cbf
Russell Salcido <russell.salcido@gmail.com>
2020-05-31 17:09:27 -0700

    Connected local module

--- index.js
+++ index.js
@@ -1,75 +1,56 @@
 const fs = require("fs");
 const axios = require("axios");
-const util = require("util");
 const inquirer = require("inquirer");
+const util = require("util");
+const generateMarkdown = require("./utils/generateMarkdown");
+const writeFileAsync = util.promisify(fs.writeFile);
 
-inquirer.prompt([
-  {
-  type: "input",
-  name: "username",
-  message: "Enter your GitHub username:"
-  },
-  {
-  type: "input",
-  name: "title",
-  message: "Enter the title of your project:"
-  },
-  {
-  type: "input",
-  name: "description",
-  message: "Describe your project:"
-  },
-  {
-  type: "input",
-  name: "installation",
-  message: "What are the steps required to install your project?:"
-  },
-  {
-  type: "input",
-  name: "usage",
-  message: "Provide instructions and examples for use.:"
-  },
-  {
-  type: "input",
-  name: "credits",
-  message: "List your collaborators, if any, with links to their GitHub profiles. Include third-party assets and tutorials you utilized:"
-  },
-  {
-  type: "input",
-  name: "contributing",
-  message: "List your collaborators, if any, with links to their GitHub profiles. Include third-party assets and tutorials you utilized:"
-  },
-  {
-  type: "input",
-  name: "tests",
-  message: "Go the extra mile and write tests for your application. Then provide examples on how to run them:"
-  },
-]);
+function promptUser() {
+	return inquirer.prompt([
+			{
+				type: "input",
+				name: "username",
+				message: "Enter your GitHub username:",
+			},
+			{
+				type: "input",
+				name: "title",
+				message: "Enter the title of your project:",
+			},
+			{
+				type: "input",
+				name: "description",
+				message: "Describe your project:",
+			},
+			{
+				type: "input",
+				name: "installation",
+				message: "What are the steps required to install your project?:",
+			},
+			{
+				type: "input",
+				name: "usage",
+				message: "Provide instructions and examples for use.:",
+			},
+			{
+				type: "input",
+				name: "credits",
+				message:
+					"List your collaborators, if any, with links to their GitHub profiles. Include third-party assets and tutorials you utilized:",
+			},
+			{
+				type: "input",
+				name: "tests",
+				message:
+					"Go the extra mile and write tests for your application. Then provide examples on how to run them:",
+			},
+		])
+		.then(function (data) {
+			console.log(data);
+			writeFileAsync("readme.md", generateMarkdown(data)).catch(function (err) {
+				console.log(err);
+			});
+		});
+}
 
- console.log()
-  
-
-  // .then(function({ username }) {
-  //   const queryUrl = `https://api.github.com/users/${username}`;
-
-  //   axios.get(queryUrl).then(function(res) {
-  //       const name = (function(data) {
-  //           return name;
-  //       console.log(res.data.name)
-  //       console.log(res.data.email)
-  //       console.log(res.data.avatar_url)
-
-
-  //     });
-     
-      
-
-    //   fs.writeFile("read-me.md", repoNamesStr, function(err) {
-    //     if (err) {
-    //       throw err;
-    //     }
-
-    //   
-    // });
-//     });
-// });
+init()
